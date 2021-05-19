c7512d086e2b1acb9f6a35ddebc02ef106983f61
VyrekXD <vyrekxd@gmail.com>
2021-05-19 12:21:07 -0500

    Fix the fix

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ config();
 		const Data = await GetComic(browser, ImageURL, _ID, isBaraOnline).catch(e => { console.log(e); return e.message })
 		if (typeof Data !== 'string') return res.status(500).json({ message: `Se produjo un error obteniendo la imagen`, error: Data })
 
-		return res.status(200).json({ message: `Se ha publicado correctamente la imagen con id: ${_ID}`, url: `cdn.muscleboat.ga/${_ID}.png`, title: Data })
+		return res.status(200).json({ message: `Se ha publicado correctamente la imagen con id: ${_ID}`, url: `https://cdn.muscleboat.ga/${_ID}.png`, title: Data })
 	})
 
 	app.listen(process.env.PORT, () => {
